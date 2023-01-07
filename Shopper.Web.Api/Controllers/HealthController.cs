11bf746055883352428f18340edf813ec6836856
Krzysztof Ziółkowski <Ziolek071@gmail.com>
2023-01-07 09:32:54 +0100

    Add api project

---
+++ Shopper.Web.Api/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shopper.Web.Api.Controllers;
+
+[ApiController]
+[Route("api/health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Shopper API service runs successfully");
+    }
+}
